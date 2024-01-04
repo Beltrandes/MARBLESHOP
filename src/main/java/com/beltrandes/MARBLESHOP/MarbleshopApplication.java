cab142d7aa0115dc7a160bed82a3156317d4d894
Beltrandes Marques <beltsmarques@hotmail.com>
2024-01-04 16:06:31 -0300

    Project created

---
+++ src/main/java/com/beltrandes/MARBLESHOP/MarbleshopApplication.java
@@ -0,0 +1,13 @@
+package com.beltrandes.MARBLESHOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarbleshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarbleshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beltrandes/MARBLESHOP/MarbleshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beltrandes.MARBLESHOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarbleshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
